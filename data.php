047c052ba912617c77de65ac140f33bd2dc9a3d4
Ali Habibie <malizzulhaq08@gmail.com>
2022-01-08 12:47:04 +0700

    updated data

--- data.php
+++ data.php
@@ -1,12 +1,43 @@
 <?php
 include 'connection.php';
 
-$query = mysqli_query($connection, "SELECT * FROM product");
+$q = "SELECT * FROM product WHERE 1 ";
 
-$result = [];
-while ($row = mysqli_fetch_array($query)) {
-    array_push($result, [
-        'nama' => $row['nama_barang']
-    ]);
+if (empty($_GET)) {
+    $query = mysqli_query($connection, $q);
+    $result = [];
+    while ($row = mysqli_fetch_array($query)) {
+        array_push($result, [
+            'id' => $row['id'],
+            'nama' => $row['nama_barang'],
+            'harga' => $row['harga_barang'],
+            'stok' => $row['stok']
+        ]);
+    }
+} elseif (isset($_GET['id']) && $_GET['id'] !== 0) {
+    $q .= "AND id='" . $_GET['id'] . "'";
+    $query = mysqli_query($connection, $q);
+    $result = [];
+    while ($row = $query->fetch_assoc()) {
+        $result[] = [
+            'id' => $row['id'],
+            'nama' => $row['nama_barang'],
+            'harga' => $row['harga_barang'],
+            'stok' => $row['stok']
+        ];
+    }
+} elseif (isset($_GET['nama_barang']) && $_GET['nama_barang'] !== 0) {
+    $q .= "AND nama_barang LIKE '%" . $_GET['nama_barang'] . "%'";
+    $query = mysqli_query($connection, $q);
+    $result = [];
+    while ($row = mysqli_fetch_array($query)) {
+        array_push($result, [
+            'id' => $row['id'],
+            'nama' => $row['nama_barang'],
+            'harga' => $row['harga_barang'],
+            'stok' => $row['stok']
+        ]);
+    }
 }
+
 echo json_encode(['result' => $result]);
